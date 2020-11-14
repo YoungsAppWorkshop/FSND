3679cf139f73be43cf252c13d4f6a8c3bdba0817
YongJe Jung <ryviusm6@gmail.com>
2020-11-14 16:57:10 +0900

    Lesson 4-4-4. Using RBAC in Flask
    Signed-off-by: YongJe Jung <ryviusm6@gmail.com>

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -105,28 +105,35 @@ def verify_decode_jwt(token):
     }, 400)
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
-
-    return wrapper
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        print('permission not in payload')
+        abort(400)
+    if permission not in payload['permissions']:
+        print('permission not in payload[permisson]')
+        abort(403)
+    return True
 
 
-@app.route('/headers')
-@requires_auth
-def headers(payload):
-    print(payload)
-    return 'Access Granted'
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
 
 
 @app.route('/images')
-@requires_auth
+@requires_auth('get:images')
 def images(jwt):
     print(jwt)
     return 'Not implemented'
